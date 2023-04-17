b6cd91e38b3641df4a6d1eea912d51374ea3a2f9
Vishwajeet Bhardwaj <vishwajeet.b@xploresense.com>
2023-04-17 13:07:59 +0530

    final commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsnneKU6TwhAcVeDFmeYEqizDrFlgaCkM",
+  authDomain: "crypt-info.firebaseapp.com",
+  projectId: "crypt-info",
+  storageBucket: "crypt-info.appspot.com",
+  messagingSenderId: "1031304258892",
+  appId: "1:1031304258892:web:8d8c6d98179b7431eddbe9"
+};
+
+export default firebaseConfig;
